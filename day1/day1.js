e48cad22a93d40f61c4e56d7cf3c9d4e8652796e
Fredrik <fredrik96.johansson@gmail.com>
2022-12-01 09:54:42 +0100

    Solution for day1

--- day1/day1.js
+++ day1/day1.js
@@ -1,3 +1,20 @@
 const loader = require("../utils/loader");
 
-const input = loader.loadInput();
+const input = loader.loadInputRaw().map((i) => parseInt(i));
+
+const elves = [];
+let count = 0;
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i]) {
+    count += input[i];
+  } else {
+    elves.push(count);
+    count = 0;
+  }
+}
+
+const sortedElves = elves.sort((a, b) => b - a);
+
+console.log("Part 1:", sortedElves[0]);
+console.log("Part 2:", sortedElves[0] + sortedElves[1] + sortedElves[2]);
